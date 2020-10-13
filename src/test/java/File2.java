467427c42022383373e050321dd5fa491ce12014
ferhatgoktepe <ferhatgoktepe@hotmail.com>
2020-10-13 22:08:48 +0200

    Initial commit

---
+++ src/test/java/File2.java
@@ -0,0 +1,7 @@
+public class File2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Second file");
+    }
+}
---
+++ src/test/java/File3.java
@@ -0,0 +1,7 @@
+public class File3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Third File");
+    }
+}
