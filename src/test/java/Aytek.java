cc62a58f058b1603f6ef252511ed1fc541714016
aytek <aytekbilici@gmail.com>
2020-10-13 22:39:21 +0200

    second try

--- src/test/java/Aytek.java
+++ src/test/java/Aytek.java
@@ -1,5 +1,7 @@
 public class Aytek {
     public static void main(String[] args) {
         System.out.println("I am trying to collaborate with Ferhat abi :)");
+
+        System.out.println("It says there is no conflict");
     }
 }
